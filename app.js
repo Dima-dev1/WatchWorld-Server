4d4e015167172a99599b1a4199a76cef59b24764
Dima <66467881+Dima-dev1@users.noreply.github.com>
2026-06-17 16:37:29 +0100

    changed server port

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(authRoutes)
 app.use(commonRoutes)
 app.use(adminRoutes)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000')
 })
 
